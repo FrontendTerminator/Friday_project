a1321bf2693ab027293d0c5099f08969b5277d1a
FrontendTerminator <mikutishvili.koba@gmail.com>
2021-03-30 16:43:57 +0300

    Refactoring: 1. HashRouter 2. Redirect to page 404 3. constants paths are added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route} from "react-router-dom";
+import {HashRouter, Redirect, Route, Switch} from "react-router-dom";
 import {Login} from "./components/login/Login";
 import {Registration} from "./components/registration/Registration";
 import {Profile} from "./components/profile/Profile";
@@ -10,30 +10,44 @@ import {NewPassword} from "./components/newPassword/NewPassword";
 import {TestPage} from "./components/testPage/TestPage";
 import {Navigation} from "./components/navigation/Navigation";
 
+const PATH = {
+    login: "/login",
+    registration: "/registration",
+    profile: "/profile",
+    page404: "/page404",
+    passwordRecovery: "/passwordRecovery",
+    newPassword: "/newPassword",
+    testPage: "/testPage",
+    pageNotFound: "*"
+}
+
 export const App = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <div>
                 <Navigation/>
                 <div>
-                    <Route path={"/login"}
-                           render={() => <Login/>}/>
-                    <Route path={"/registration"}
-                           render={() => <Registration/>}/>
-                    <Route path={"/profile"}
-                           render={() => <Profile/>}/>
-                    <Route path={"/page404"}
-                           render={() => <Page404/>}/>
-                    <Route path={"/passwordRecovery"}
-                           render={() => <PasswordRecovery/>}/>
-                    <Route path={"/newPassword"}
-                           render={() => <NewPassword/>}/>
-                    <Route path={"/testPage"}
-                           render={() => <TestPage/>}/>
+                    <Switch>
+                        <Route path={PATH.login}
+                               render={() => <Login/>}/>
+                        <Route path={PATH.registration}
+                               render={() => <Registration/>}/>
+                        <Route path={PATH.profile}
+                               render={() => <Profile/>}/>
+                        <Route path={PATH.page404}
+                               render={() => <Page404/>}/>
+                        <Route path={PATH.passwordRecovery}
+                               render={() => <PasswordRecovery/>}/>
+                        <Route path={PATH.newPassword}
+                               render={() => <NewPassword/>}/>
+                        <Route path={PATH.testPage}
+                               render={() => <TestPage/>}/>
+                        <Redirect from={PATH.pageNotFound} to={PATH.page404}/>
+                    </Switch>
                 </div>
             </div>
 
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
