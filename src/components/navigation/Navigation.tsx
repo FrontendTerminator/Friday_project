11bba27182528c4c2b302cbae47e56c1b4f3c7d2
FrontendTerminator <mikutishvili.koba@gmail.com>
2021-04-05 15:49:52 +0300

    Navigation.tsx is refactored

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import s from "./Navigation.module.css"
+import {PATH} from "../../App";
 
 export const Navigation = () => {
     return (
         <div className={s.navigationBlock}>
-            <NavLink to={"/login"}>login</NavLink>
-            <NavLink to={"/registration"}>registration</NavLink>
-            <NavLink to={"/profile"}>profile</NavLink>
-            <NavLink to={"/page404"}>page 404</NavLink>
-            <NavLink to={"/passwordRecovery"}>password recovery</NavLink>
-            <NavLink to={"/newPassword"}>new password</NavLink>
-            <NavLink to={"/testPage"}>test page</NavLink>
+            <NavLink to={PATH.login}>login</NavLink>
+            <NavLink to={PATH.registration}>registration</NavLink>
+            <NavLink to={PATH.profile}>profile</NavLink>
+            <NavLink to={PATH.page404}>page 404</NavLink>
+            <NavLink to={PATH.passwordRecovery}>password recovery</NavLink>
+            <NavLink to={PATH.newPassword}>new password</NavLink>
+            <NavLink to={PATH.testPage}>test page</NavLink>
         </div>
     )
 }
